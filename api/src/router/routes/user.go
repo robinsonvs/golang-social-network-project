27eee1391dd137d5216a3690f05c7b650279311b
robinsonvs <robinsonvs@gmail.com>
2021-11-28 02:55:43 -0300

    add routes

---
+++ api/src/router/routes/user.go
@@ -0,0 +1,54 @@
+package routes
+
+import "net/http"
+
+var routesUsers = []Route{
+	{
+		URI:    "/users",
+		Method: http.MethodPost,
+		Function: func(w http.ResponseWriter, r *http.Request) {
+
+		},
+		RequiresAuthentication: false,
+	},
+	{
+		URI:    "/users",
+		Method: http.MethodPost,
+		Function: func(w http.ResponseWriter, r *http.Request) {
+
+		},
+		RequiresAuthentication: false,
+	},
+	{
+		URI:    "/users",
+		Method: http.MethodPost,
+		Function: func(w http.ResponseWriter, r *http.Request) {
+
+		},
+		RequiresAuthentication: false,
+	},
+	{
+		URI:    "/users",
+		Method: http.MethodPost,
+		Function: func(w http.ResponseWriter, r *http.Request) {
+
+		},
+		RequiresAuthentication: false,
+	},
+	{
+		URI:    "/users",
+		Method: http.MethodPost,
+		Function: func(w http.ResponseWriter, r *http.Request) {
+
+		},
+		RequiresAuthentication: false,
+	},
+	{
+		URI:    "/users",
+		Method: http.MethodPost,
+		Function: func(w http.ResponseWriter, r *http.Request) {
+
+		},
+		RequiresAuthentication: false,
+	},
+}
